7d3bda3394fd7745b93f62100c265ad350105051
Wendels1 <ferreirawendel774@gmail.com>
2025-11-21 21:34:30 -0300

    Ajustando o script de cursos

--- src/controllers/cursoController.js
+++ src/controllers/cursoController.js
@@ -6,16 +6,16 @@ const { validarCurso, validarAtualizacaoCurso } = require('../validators/cursoVa
 const { validarId } = require('../validators/IDValidator');
 
 router.get('/cursos', async (req, res) => {
-  const cursos = await cursoModel.find().populate(['departamento']);
+  const cursos = await cursoModel.find().populate('departamento');
   res.json(cursos);
 });
 
 router.get('/cursos/:id', validarId, async (req, res) => {
-  const cursos = await cursoModel.findById(req.params.id).populate(['departamento']);
-  if (!cursos) {
+  const curso = await cursoModel.findById(req.params.id).populate('departamento');
+  if (!curso) {
     return res.status(404).json({ error: 'Curso não encontrado' });
   }
-  res.json(cursos);
+  res.json(curso);
 });
 
 router.post('/cursos', validarCurso, async (req, res) => {
@@ -24,17 +24,23 @@ router.post('/cursos', validarCurso, async (req, res) => {
 });
 
 router.put('/cursos/:id', validarId, validarAtualizacaoCurso, async (req, res) => {
-  const cursoAtualizado = await cursoModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  const cursoAtualizado = await cursoModel.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
   if (!cursoAtualizado) {
     return res.status(404).json({ error: 'Curso não encontrado' });
   }
+
   res.json(cursoAtualizado);
 });
 
 router.delete('/cursos/:id', validarId, async (req, res) => {
   const cursoDeletado = await cursoModel.findByIdAndDelete(req.params.id);
   if (!cursoDeletado) {
-    return res.status(404).json({ error: 'Funcionário não encontrado' });
+    return res.status(404).json({ error: 'Curso não encontrado' });
   }
   res.status(204).send();
 });
